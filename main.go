941cef7d94134e6abbe5020a5341e8a3bea72deb
HaiHart <hai.hohartfilli@hcmut.edu.vn>
2023-02-23 16:27:10 +0700

    server 1st ver

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
-package server
+package main
 
 import (
 	"context"
 	"fmt"
 	"io"
-	"log"
+	"net"
+	"sync"
+
+	// "log"
 	"time"
 
 	pb "github.com/HaiHart/ShipdockServer/proto"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/keepalive"
+
+	// "google.golang.org/grpc/credentials"
+	// "google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	// grpc "google.golang.org/grpc"
@@ -45,6 +50,7 @@ type SerConn struct {
 	cache       []Container
 	clients     map[string]RelayConn
 	toSend      map[string]chan *pb.Pack
+	lock        sync.Mutex
 	currCommand []Container
 	port        int32
 }
@@ -79,31 +85,51 @@ func (s *SerConn) MoveContainer(msg pb.Com_MoveContainerServer) error {
 			return err
 		}
 		var changes = Container{
-			Name:   in.List.Name,
-			Placed: (in.List.Place),
+			Name:   in.List[0].Name,
+			Placed: (in.List[0].Place),
 			Key:    0,
-			Iden:   in.List.Id,
-			inTime: in.List.Time.AsTime(),
+			Iden:   in.List[0].Id,
+			inTime: in.List[0].Time.AsTime(),
 		}
-		s.Valid(&changes)
+		var new_place = in.List[0].NewPlace
+		var swap = in.Swap
+		if swap {
+			var changes_2 = Container{
+				Name:   in.List[1].Name,
+				Placed: (in.List[1].Place),
+				Key:    0,
+				Iden:   in.List[1].Id,
+				inTime: in.List[1].Time.AsTime(),
+			}
+			s.ValidSwap(&changes, &changes_2)
+		}
+
+		s.ValidMove(&changes, new_place)
 	}
 
 	return nil
 }
 
-func (s *SerConn) Valid(changes *Container) {
+func (s *SerConn) ValidMove(changes *Container, new_place int32) {
+
+	if changes == nil {
+		return
+	}
 
 	var new_move = &pb.Pack{
-		List: &pb.Container{
-			Name:  changes.Name,
-			Id:    changes.Iden,
-			Place: changes.Placed,
-			Time:  timestamppb.New(changes.inTime),
+		List: []*pb.Container{
+			{
+				Name:     changes.Name,
+				Id:       changes.Iden,
+				Place:    changes.Placed,
+				Time:     timestamppb.New(changes.inTime),
+				NewPlace: new_place,
+			},
 		},
 	}
 
-	s.currCommand = append([]Container{*changes}, s.currCommand...)
-	if s.CheckOnCache(changes) {
+	if s.CheckOnCacheMove(changes, new_place) {
+		s.currCommand = append([]Container{*changes}, s.currCommand...)
 		for _, i := range s.toSend {
 			i <- new_move
 		}
@@ -112,10 +138,128 @@ func (s *SerConn) Valid(changes *Container) {
 	return
 }
 
-func (s *SerConn) CheckOnCache(changes *Container) bool {
+func (s *SerConn) ValidSwap(changes *Container, changes_2 *Container) {
+	if changes == nil || changes_2 == nil {
+		return
+	}
+
+	var new_move = &pb.Pack{
+		List: []*pb.Container{
+			{
+				Name:     changes.Name,
+				Id:       changes.Iden,
+				Place:    changes.Placed,
+				Time:     timestamppb.New(changes.inTime),
+				NewPlace: changes_2.Placed,
+			},
+			{
+				Name:     changes_2.Name,
+				Id:       changes_2.Iden,
+				Place:    changes_2.Placed,
+				Time:     timestamppb.New(changes_2.inTime),
+				NewPlace: changes.Placed,
+			},
+		},
+	}
+
+	if s.CheckOnCacheSwap(changes, changes_2) {
+		s.currCommand = append([]Container{*changes, *changes_2}, s.currCommand...)
+		for _, i := range s.toSend {
+			i <- new_move
+		}
+	}
+
+	return
+}
+
+func (s *SerConn) CheckOnCacheMove(changes *Container, new_place int32) bool {
+
+	if changes == nil {
+		return false
+	}
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	for _, v := range s.cache {
+		if v.Placed == new_place && new_place != -1 {
+			return false
+		}
+	}
+	for _, v := range s.cache {
+		if v.Iden == changes.Iden {
+			if v.Placed != changes.Placed {
+				return false
+			}
+			v.Placed = new_place
+		}
+	}
+	return true
+}
+
+func (s *SerConn) CheckOnCacheSwap(changes *Container, changes_2 *Container) bool {
+	if changes == nil || changes_2 == nil {
+		return false
+	}
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	for _, v := range s.cache {
+		if v.Iden == changes.Iden && v.Placed != changes.Placed {
+			return false
+		}
+		if v.Iden == changes_2.Iden && v.Placed != changes_2.Placed {
+			return false
+		}
+	}
+	for _, v := range s.cache {
+		if v.Iden == changes.Iden {
+			v.Placed = changes_2.Placed
+		}
+		if v.Iden == changes_2.Iden {
+			v.Placed = changes.Placed
+		}
+	}
 	return true
 }
 
 func (s *SerConn) FetchShip(ctx context.Context, msg *pb.ShipAccess) (*pb.ShipResponse, error) {
 	return nil, nil
+}
+
+func (s *SerConn) Start() error {
+	ln, _ := net.Listen("tcp", fmt.Sprintf("localhost:%v", s.port))
+	var opts []grpc.ServerOption
+	var grpcServer = grpc.NewServer(opts...)
+	pb.RegisterComServer(grpcServer, s)
+	err := grpcServer.Serve(ln)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *SerConn) RunServer() error {
+	defer s.cancel()
+	var err error = s.Start()
+
+	return err
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	server := SerConn{
+		port:    8080,
+		context: ctx,
+		cancel:  cancel,
+	}
+	var group errgroup.Group
+	fmt.Println("here")
+	group.Go(server.RunServer)
+	fmt.Println("Started")
+
+	err := group.Wait()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
